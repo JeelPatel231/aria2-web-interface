9b1373928132d5fceb1ed92bebff8bfd8fedc232
jeelpatel231 <jeelpatel231@gmail.com>
2022-07-19 00:09:15 +0530

    remove redundant code and add tellStatus request

--- src/store/websockutils.ts
+++ src/store/websockutils.ts
@@ -66,19 +66,13 @@ export class AriaWebSocket {
         this.WS.send(JSON.stringify(json));
     };
 
-    pauseDownload = (gid) => {
-        this.wsreq('pause','aria2.pause',[gid])
-        this.tellActive()
-    }
+    pauseDownload = (gid) => this.wsreq('pause','aria2.pause',[gid])
 
-    unpauseDownload = (gid) => {
-        this.wsreq('unpause','aria2.unpause',[gid])
-        this.tellWaiting()
-    }
+    unpauseDownload = (gid) => this.wsreq('unpause','aria2.unpause',[gid])
 
-    removeDownload = (gid) => {
-        this.wsreq('remove','aria2.remove',[gid])
-    }
+    removeDownload = (gid) => this.wsreq('remove','aria2.remove',[gid])
+
+    tellStatus = (gid) => this.wsreq('tellstatus','aria2.tellStatus', [gid])
 
     // CALLS START HERE
     tellActive = () => { this.wsreq('tellactive','aria2.tellActive',
